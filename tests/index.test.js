a71d9864f79ae6f64cc29cfdd8d03464596ff0be
bjwangyinan <bjwangyinan@corp.netease.com>
2017-10-05 10:26:44 +0800

    remove mocha

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { shallow } from 'enzyme';
-import { expect } from 'chai';
 import ImageSlides from '../src/ImageSlides';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -13,13 +12,13 @@ const images = [
   'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1503235534249&di=4c198d5a305627d12e5dae4c581c9e57&imgtype=0&src=http%3A%2F%2Fimg2.niutuku.com%2Fdesk%2Fanime%2F0529%2F0529-17277.jpg',
 ];
 
-describe('<ImageSlides />', function () {
-  it('renders two imgs when then index is equal to 0', function () {
+describe('<ImageSlides />', () => {
+  it('renders two imgs when then index is equal to 0', () => {
     const wrapper = shallow(<ImageSlides images={images} isOpen />);
-    expect(wrapper.find('.image-slides-blackboard')).to.have.length(2);
+    expect(wrapper.find('.image-slides-blackboard').length).toBe(2);
   });
-  it('renders three imgs when then index is equal to 2', function () {
+  it('renders three imgs when then index is equal to 2', () => {
     const wrapper = shallow(<ImageSlides images={images} index={2} isOpen />);
-    expect(wrapper.find('.image-slides-blackboard')).to.have.length(3);
+    expect(wrapper.find('.image-slides-blackboard').length).toBe(3);
   });
 });
