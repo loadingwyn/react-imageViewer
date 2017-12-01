39a5b37b6245a1efc023a932c9b8ab3cd836005b
bjwangyinan <bjwangyinan@corp.netease.com>
2017-12-01 16:19:35 +0800

    更新依赖

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: [
     // 'react-hot-loader/patch',
     // 'webpack-dev-server/client?http://localhost:8080',
     // 'webpack/hot/only-dev-server',
-    process.env.NODE_ENV === 'production' ? './src/index.js' : './demo/mobile/demo.js',
+    process.env.NODE_ENV === 'production'
+      ? './src/index.js'
+      : './demo/mobile/demo.js',
   ],
   output: {
     path: resolve(__dirname, 'dist'),
@@ -15,42 +18,52 @@ module.exports = {
     filename: 'bundle.js',
   },
   devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
-  externals: process.env.NODE_ENV === 'production' ? [
-    'react',
-    'react-dom',
-    'prop-types',
-    'alloyfinger',
-  ] : [],
+  externals:
+    process.env.NODE_ENV === 'production'
+      ? ['react', 'react-dom', 'prop-types', 'alloyfinger']
+      : [],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
-        options: {
-          sourceMap: process.env.NODE_ENV !== 'production',
-          minimize: true,
-          importLoaders: 1,
-        },
-      }, 'postcss-loader'],
-    }],
-  },
-  plugins: process.env.NODE_ENV === 'production' ? [
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      comments: false,
-      compress: {
-        warnings: false,
-        drop_console: true,
-        collapse_vars: true,
-        reduce_vars: true,
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
       },
-    }),
-  ] : [],
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production',
+              minimize: true,
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+        new webpack.optimize.UglifyJsPlugin({
+          beautify: false,
+          comments: false,
+          compress: {
+            warnings: false,
+            drop_console: true,
+            collapse_vars: true,
+            reduce_vars: true,
+          },
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        // new BundleAnalyzerPlugin(),
+      ]
+      : [],
 };
