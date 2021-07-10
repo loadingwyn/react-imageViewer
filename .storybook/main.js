c3e130ce9dd11e4a66f371a1f03b445531a1cae9
loadingwyn <wyn153409018@outlook.com>
2021-07-10 18:28:11 +0800

    update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 const path = require('path');
 
-// your app's webpack.config.js
-const custom = require('../webpack.config.js');
-
 module.exports = {
   stories: ['../stories/**/*.@(tsx|jsx)'],
 };
