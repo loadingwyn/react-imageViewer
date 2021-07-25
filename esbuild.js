b4d1166596f2a0dd7b1d455f47fcaa0049decb88
loadingwyn <wyn153409018@outlook.com>
2021-07-25 20:38:37 +0800

    update esbuild script

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,21 @@
 const esbuild = require('esbuild');
+const path = require('path');
 
+// eslint-disable-next-line import/no-dynamic-require
+const pkg = require(path.resolve('./package.json'));
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
 esbuild
   .build({
     entryPoints: ['src/index.ts'],
     outdir: 'lib',
     bundle: true,
     sourcemap: true,
-    minify: true,
-    splitting: true,
     format: 'esm',
     target: ['esnext'],
+    external,
   })
   .catch(() => process.exit(1));
--- tests/index.test.jsx
+++ tests/index.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
-import Gallery from '../src/Gallery';
+import Gallery from '../lib/index';
 
 let container = null;
 const images = [
